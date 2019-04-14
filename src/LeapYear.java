58f5701604f25a33642e4b9b6afb43d38c858010
Aligned Ginger <15883219+stavroschios@users.noreply.github.com>
2019-04-14 13:09:55 -0400

    Update LeapYear.java

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -2,7 +2,7 @@ public class LeapYear {
 
 
     public static boolean isLeapYear(int year) {
-        if ((year >= 0) && (year <= 9999)) {
+        if ((year >= 1) && (year <= 9999)) {
 
              if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
 
