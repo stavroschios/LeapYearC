ad689e866dfed6e9812a95248b297f622e2d62be
Aligned Ginger <stabrospax@hotmail.com>
2019-04-14 13:03:19 -0400

    See if the year entered is a Leap Year or not

---
+++ src/LeapYear.java
@@ -0,0 +1,22 @@
+public class LeapYear {
+
+
+    public static boolean isLeapYear(int year) {
+        if ((year >= 0) && (year <= 9999)) {
+
+             if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+
+                return true;
+
+            } else {
+                return false;
+
+            }
+        } else {
+            return false;
+        }
+
+
+    }
+
+}
