034ea4da02d3ac175cde35a3fc8e2543911616ec
Stavros Pachoundakis <15883219+stavroschios@users.noreply.github.com>
2019-05-04 22:48:42 -0400

    Update LeapYear.java

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -2,21 +2,13 @@ public class LeapYear {
 
 
     public static boolean isLeapYear(int year) {
-        if ((year >= 1) && (year <= 9999)) {
-
-             if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-
-                return true;
-
-            } else {
-                return false;
-
-            }
-        } else {
+         if(year < 1 || year > 9999) {
             return false;
+        } else if(year >= 1 && year <= 9999) {
+            if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+                return true;
+            }
         }
-
-
+        return false;
     }
-
 }
